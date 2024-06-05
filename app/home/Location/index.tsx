d5bdc08d3c7486ab1c6050dbfbedfa6cc07f18fe
narol <narolcat@gmail.com>
2024-06-05 17:04:09 +0800

    fix: The location may be updated multiple times

--- app/home/Location/index.tsx
+++ app/home/Location/index.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { observer } from "mobx-react-lite";
 import { useStores } from "@/hooks/useStores";
 import { useGeolocation } from "@uidotdev/usehooks";
 
 export const Location = observer(() => {
+  const hasLocated = useRef(false);
   const { longitude, latitude } = useGeolocation();
   const { user } = useStores();
   useEffect(() => {
-    if (longitude && latitude) {
+    if (longitude && latitude && !hasLocated.current) {
       user.updateLocation(longitude, latitude);
+      hasLocated.current = true;
     }
   }, [longitude, latitude]);
   return <></>;
